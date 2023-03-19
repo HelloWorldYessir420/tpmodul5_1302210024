660dcb1a99cac00b654edf2a8abd4934f0808414
BlyatMan221 <119160536+BlyatMan221@users.noreply.github.com>
2023-03-19 23:11:37 +0700

    main program

---
+++ tpmodul5_1302210024/tpmodul5_1302210024/Program.cs
@@ -0,0 +1,21 @@
+﻿using tpmodul5_1302210024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        string nama = "praktikan";
+        halo.SapaUser(nama);
+
+        DataGeneric<string> data = new DataGeneric<string>("");
+
+        Console.Write("Masukkan NIM: ");
+        string nim = Console.ReadLine();
+        data = new DataGeneric<string>(nim);
+
+        data.PrintData();
+
+        Console.ReadKey();
+    }
+}
