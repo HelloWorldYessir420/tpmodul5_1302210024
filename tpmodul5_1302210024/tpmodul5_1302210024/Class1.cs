5013540d90c0d9e182b21826e238fc2d4a1657f7
BlyatMan221 <119160536+BlyatMan221@users.noreply.github.com>
2023-03-19 23:10:34 +0700

    menambahkan class HaloGeneric dengan method  SapaUser

---
+++ tpmodul5_1302210024/tpmodul5_1302210024/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302210024
+{
+    internal class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+}
